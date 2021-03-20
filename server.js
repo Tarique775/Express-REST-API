077f5d27fb4a0f7b0da952e132988276bd6497f9
tarikul islam <islam.tarique4329@gmail.com>
2021-03-21 00:48:45 +0600

    changed something

--- server.js
+++ server.js
@@ -38,8 +38,9 @@ app.get('/', (req, res) => {
     res.send('hello world');
 });
 
-app.use((err, req, res, next) => {
-    next('Requested url was not found!');
+app.use((req, res, next) => {
+    res.status(404).json('Requested Url was not found!');
+    next("Requested url wasn't match!");
 });
 
 app.use((err, req, res, next) => {
